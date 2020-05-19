2b48518efec48265580a80da60ddca34274f7fcb
ahmedswa68 <ahmedswar68@gmail.com>
2020-05-19 22:36:26 +0200

    form modal

--- src/modules/users/components/Users.js
+++ src/modules/users/components/Users.js
@@ -1,76 +1,82 @@
-import React from 'react';
-import { translatedTitle } from 'reactor/metadata';
-import Table from 'shared/components/table/table';
-import LinearProgress from '@material-ui/core/LinearProgress';
-import usersService from 'modules/users/services/users-service';
-import ReactorComponent from 'reactor/component/reactor.component';
-import DashboardLayout from 'shared/components/layout/dashboard-layout';
-import { TableEditButton, TableDeleteButton } from 'shared/components/table/table-actions';
+import React from "react";
+import { translatedTitle } from "reactor/metadata";
+import Table from "shared/components/table/table";
+import LinearProgress from "@material-ui/core/LinearProgress";
+import usersService from "modules/users/services/users-service";
+import ReactorComponent from "reactor/component/reactor.component";
+import DashboardLayout from "shared/components/layout/dashboard-layout";
+import {
+  TableEditButton,
+  TableDeleteButton,
+} from "shared/components/table/table-actions";
 
 export default class Users extends ReactorComponent {
-    state = {
-        isLoading: true,
-    };
+  state = {
+    isLoading: true,
+  };
 
-    table = {
-        heading: 'users',
-        columns: [
-            {
-                heading: '#',
-                key: 'id',
-            },
-            {
-                heading: 'name',
-                key: 'name',
-            },
-            {
-                heading: 'group',
-                key: 'group.name',
-            },
-            {
-                heading: 'email',
-                key: 'email',
-            },
-            {
-                heading: 'actions',
-                buttons: [TableEditButton, TableDeleteButton]
-            }
-        ],
-    };
+  table = {
+    heading: "users",
+    columns: [
+      {
+        heading: "#",
+        key: "id",
+      },
+      {
+        heading: "name",
+        key: "name",
+      },
+      {
+        heading: "group",
+        key: "group.name",
+      },
+      {
+        heading: "email",
+        key: "email",
+      },
+      {
+        heading: "actions",
+        buttons: [TableEditButton, TableDeleteButton],
+      },
+    ],
+  };
 
-    /**
-     * {@inheritdoc}
-     */
-    async init() {
-        translatedTitle(this.table.heading);
+  /**
+   * {@inheritdoc}
+   */
+  async init() {
+    translatedTitle(this.table.heading);
 
-        let { data } = await usersService.list(); // /users
+    let { data } = await usersService.list(); // /users
 
-        let { records, paginationInfo } = data;
+    let { records, paginationInfo } = data;
 
-        this.paginationInfo = paginationInfo;
+    this.paginationInfo = paginationInfo;
 
-        this.records = records;
+    this.records = records;
 
-        this.set('isLoading', false);
-    }
+    this.set("isLoading", false);
+  }
 
-    renderTable() {
-        return <Table
-            options={this.table}
-            records={this.records}
-            pagination={this.paginationInfo} />
-    }
+  renderTable() {
+    return (
+      <Table
+        options={this.table}
+        records={this.records}
+        pagination={this.paginationInfo}
+      />
+    );
+  }
 
-    /**
-     * {@inheritdoc}
-     */
-    render() {
-        let displayedContent = this.get('isLoading') ? <LinearProgress /> : this.renderTable();
-        return (
-            <DashboardLayout>
-                {displayedContent}
-            </DashboardLayout>
-        );
-    }
-}   
+  /**
+   * {@inheritdoc}
+   */
+  render() {
+    let displayedContent = this.get("isLoading") ? (
+      <LinearProgress />
+    ) : (
+      this.renderTable()
+    );
+    return <DashboardLayout>{displayedContent}</DashboardLayout>;
+  }
+}
--- src/shared/components/layout/dashboard-layout.js
+++ src/shared/components/layout/dashboard-layout.js
@@ -1,47 +1,66 @@
-import clsx from 'clsx';
-import React from 'react';
+import clsx from "clsx";
+import React from "react";
 import Layout from "./layout";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 import Header from "../header/header";
+import FormModal from "./form-modal";
 import Sidebar from "../sidebar/sidebar";
+import { Button } from "@material-ui/core";
 import layoutSettings from "../layout-settings";
 
 export default function DashboardLayout(props) {
-    const classes = layoutSettings();
-    const [open, setOpen] = React.useState(false);
+  const classes = layoutSettings();
+  const [open, setOpen] = React.useState(false);
+  const [modalIsOpened, toggleModal] = React.useState(false);
 
-    const handleDrawerOpen = () => {
-        setOpen(true);
-    };
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
 
-    const handleDrawerClose = () => {
-        setOpen(false);
-    };
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
 
-    return (
-        <Layout>
-            <div className={classes.root}>
-                <Header
-                    sidebarIsOpened={open}
-                    onClick={handleDrawerOpen}
-                />
-                <Sidebar
-                    open={open}
-                    onClose={handleDrawerClose}
-                />
-                <main
-                    className={clsx(classes.content, {
-                        [classes.contentShift]: open,
-                    })}
-                >
-                    <div className={classes.drawerHeader} />
-                    {props.children}
-                </main>
-            </div>
-        </Layout>
-    );
+  const openModal = () => {
+    toggleModal(true);
+  };
+
+  const closeModal = () => {
+    toggleModal(false);
+  };
+
+  return (
+    <Layout>
+      <div className={classes.root}>
+        <Header sidebarIsOpened={open} onClick={handleDrawerOpen} />
+        <Sidebar open={open} onClose={handleDrawerClose} />
+        <main
+          className={clsx(classes.content, {
+            [classes.contentShift]: open,
+          })}
+        >
+          <div className={classes.drawerHeader} />
+
+          <Button variant="outlined" color="primary" onClick={openModal}>
+            Open full-screen dialog
+          </Button>
+
+          <FormModal
+            title="Create new user"
+            onSubmit={closeModal}
+            open={modalIsOpened}
+            onClose={closeModal}
+          >
+            <h1>Welcome Modal</h1>
+          </FormModal>
+
+          {props.children}
+        </main>
+      </div>
+    </Layout>
+  );
 }
 
 DashboardLayout.propTypes = {
-    children: PropTypes.any.isRequired,
+  children: PropTypes.any.isRequired,
 };
